1403e3045f8d3469cacded0bd124669e2a48914a
muchwat <kevinmuchwat@gmail.com>
2023-12-08 13:36:30 +0300

    update throttle mechanism for monitoring server health

--- commands/monitor.php
+++ commands/monitor.php
@@ -17,18 +17,18 @@ class Monitor extends Command
     public function handle()
     {
         try {
-            $mockLogger = app(MockLogger::class);
-
             $monitorValues = MonitorManagerService::getValues();
+            $thresholdExceeded = $this->exceedsThreshold($monitorValues);
 
-            if ($this->exceedsThreshold($monitorValues)) {
+            if ($thresholdExceeded) {
+                $monitorValues['hard_disk_space'] = $this->calculateHddPercentage($monitorValues['hard_disk_space']);
                 $this->sendNotificationEmail($monitorValues);
             } else {
                 $this->resetCache();
             }
 
-            $response = $mockLogger->sendLogData(['monitor_values' => $monitorValues]);
-
+            $appName = config('app.name');
+            $response = $this->sendMockLoggerData($monitorValues, $appName);
             $this->outputResponseDetails($response);
 
             $this->info('Data sent successfully.');
@@ -40,74 +40,68 @@ class Monitor extends Command
     protected function exceedsThreshold(array $monitorValues): bool
     {
         $thresholds = Config::get('mocklogger.monitor.thresholds');
-
-        $monitorValues['cpu_usage'] = 100;
-        $monitorValues['memory_usage'] = 100;
-        $monitorValues['hard_disk_space'] = [
-            'freeSpace' => 100,
-            'totalSpace' => 100,
-            'unit' => 'GB',
-        ];
-
-        $hddSpace = $monitorValues['hard_disk_space'];
-        $hardDiskSpace = ($hddSpace['freeSpace'] / ($hddSpace['totalSpace'] ?? 0)) * 100;
-
-        return (
-            ($monitorValues['cpu_usage'] ?? 0) > $thresholds['cpu_usage'] ||
-            ($monitorValues['memory_usage'] ?? 0) > $thresholds['memory_usage'] ||
-            ($hardDiskSpace ?? 0) > $thresholds['hard_disk_space']
-        );
+        return $this->cpuExceeded($monitorValues, $thresholds) ||
+            $this->memoryExceeded($monitorValues, $thresholds) ||
+            $this->hddExceeded($monitorValues, $thresholds);
     }
 
     protected function sendNotificationEmail(array $monitorValues)
     {
         $adminEmail = Config::get('mocklogger.monitor.email.admin');
-
         if (!is_null($adminEmail)) {
-            $appName = config('app.name');
-            $emailCount = Config::get('mocklogger.monitor.email.count');
-            $emailInterval = Config::get('mocklogger.monitor.email.interval');
-
-            $subject = "$appName - Server Resource Threshold Exceeded";
-
-            $message = "Server ($appName) resources have exceeded predefined thresholds:\n" .
-                "CPU: {$monitorValues['cpu_usage']}%\n" .
-                "Memory: {$monitorValues['memory_usage']}%\n" .
-                "Hard Disk: {$monitorValues['hard_disk_space']}%";
-
-            $this->sendEmailIfNeeded($adminEmail, $subject, $message, $emailCount, $emailInterval);
+            $this->sendThresholdEmail($adminEmail, $monitorValues);
         }
     }
 
-    protected function sendEmailIfNeeded($adminEmail, $subject, $message, $emailCount, $emailInterval)
+    protected function sendMockLoggerData(array $monitorValues, string $appName): array
     {
-        if (Cache::get('mocklogger.monitor.email.interval', false)) {
-            $currentEmailCount = Cache::increment('email_count');
-
-            if ($currentEmailCount <= $emailCount) {
-                Mail::raw($message, function ($message) use ($adminEmail, $subject) {
-                    $message->to($adminEmail)->subject($subject);
-                });
-            }
-        } else {
-            $this->resetCache($emailInterval);
-        }
+        $mockLogger = app(MockLogger::class);
+        return $mockLogger->sendLogData([
+            'monitor_values' => $monitorValues, 
+            'app_name' => $appName],
+        );
     }
 
-    protected function resetCache($emailInterval = null)
+    private function calculateHddPercentage(array $hddSpace): float
     {
-        Cache::forget('email_count');
-
-        if (is_null($emailInterval)) {
-            return Cache::forget('mocklogger.monitor.email.interval');
-        }
-
-        Cache::put('mocklogger.monitor.email.interval', true, now()->addMinutes($emailInterval));
+        return ($hddSpace['freeSpace'] / ($hddSpace['totalSpace'] ?? 0)) * 100;
     }
 
-    protected function outputResponseDetails($response)
+    private function cpuExceeded(array $monitorValues, array $thresholds): bool
     {
-        $this->line('Response Status Code: ' . $response->status());
-        $this->line('Response Body: ' . $response->body());
+        return ($monitorValues['cpu_usage'] ?? 0) > $thresholds['cpu_usage'];
+    }
+
+    private function memoryExceeded(array $monitorValues, array $thresholds): bool
+    {
+        return ($monitorValues['memory_usage'] ?? 0) > $thresholds['memory_usage'];
+    }
+
+    private function hddExceeded(array $monitorValues, array $thresholds): bool
+    {
+        return $this->calculateHddPercentage($monitorValues) > $thresholds['hard_disk_space'];
+    }
+
+    private function sendThresholdEmail(string $adminEmail, array $monitorValues): void
+    {
+        $appName = config('app.name');
+        $message = $this->buildThresholdEmailMessage($monitorValues, $appName);
+        $this->sendEmail($adminEmail, $appName, $message);
+    }
+
+    private function buildThresholdEmailMessage(array $monitorValues, string $appName): string
+    {
+        return "Server ($appName) resources have exceeded predefined thresholds:\n" .
+            "CPU: {$monitorValues['cpu_usage']}%\n" .
+            "Memory: {$monitorValues['memory_usage']}%\n" .
+            "Hard Disk: {$this->calculateHddPercentage($monitorValues)}%";
+    }
+
+    private function sendEmail(string $adminEmail, string $appName, string $message): void
+    {
+        $subject = "$appName - Server Resource Threshold Exceeded";
+        Mail::raw($message, function ($message) use ($adminEmail, $subject) {
+            $message->to($adminEmail)->subject($subject);
+        });
     }
 }
