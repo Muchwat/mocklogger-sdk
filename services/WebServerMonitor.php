694db94000d24c7093dc04e60021169162b0b02a
muchwat <kevinmuchwat@gmail.com>
2023-12-15 13:05:24 +0300

    update

--- services/WebServerMonitor.php
+++ services/WebServerMonitor.php
@@ -10,17 +10,50 @@ namespace Moktech\MockLoggerSDK\Services;
 class WebServerMonitor
 {
     /**
-     * Get the server's IP address
+     * Get the server IP address based on the operating system.
      *
-     * @return string The  server's IP address
+     * @return string|null The server IP address or null if it couldn't be determined.
      */
     public static function getIpAddress(): ?string
-    {   
-        if (PHP_OS !== 'Linux') {
-            return null;
+    {
+        if (PHP_OS === 'Linux') {
+            return self::getLinuxIpAddress();
         }
 
-        // Execute the command to get the server's IP address
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            return self::getWindowsIpAddress();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the server IP address on Windows.
+     *
+     * @return string|null The server IP address or null if it couldn't be determined.
+     */
+    private static function getWindowsIpAddress(): ?string
+    {
+        // Execute 'ipconfig' command to get IP address on Windows
+        $output = shell_exec('ipconfig');
+
+        // Extract IPv4 address from the output
+        preg_match_all('/IPv4 Address[^:]*:\s*([^\r\n]+)/', $output, $matches);
+
+        // Use the first match as the IP address
+        $ipAddress = isset($matches[1][0]) ? trim($matches[1][0]) : null;
+
+        return $ipAddress;
+    }
+
+    /**
+     * Get the server IP address on Linux.
+     *
+     * @return string|null The server IP address or null if it couldn't be determined.
+     */
+    private static function getLinuxIpAddress(): ?string
+    {
+        // Execute 'hostname -I' command to get IP address on Linux
         $serverIpAddress = shell_exec('hostname -I');
 
         // The 'hostname -I' command usually returns a list of IP addresses; extract the first one
@@ -30,35 +63,17 @@ class WebServerMonitor
     }
 
     /**
-     * Get the status of the web server.
-     *
-     * @return string The status of the web server.
-     */
-    public static function getValue(): ?string
-    {
-        if (PHP_OS !== 'Linux') {
-            return null;
-        }
-
-        // Get the configured web server name
-        $webServerName = config('mocklogger.monitor.web_server');
-
-        // Check the status of the web server
-        return self::checkStatus($webServerName);
-    }
-
-    /**
      * Check the status of a specific web server.
      *
      * @param string $server The name of the web server.
      * @return string The status of the web server.
      */
     protected static function checkStatus(string $server): ?string
-    {   
+    {
         if (PHP_OS !== 'Linux') {
             return null;
         }
-        
+
         // Command to check the status of the web server using systemctl
         $command = "systemctl is-active $server.service";
 
